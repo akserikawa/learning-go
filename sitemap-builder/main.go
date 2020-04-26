bf98f89f94f071778bc9b9f51b097772141a2049
Akira Serikawa <akserikawa@gmail.com>
2020-04-26 19:41:28 +0200

    GET-ting a Webpage

--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to be a sitemap for")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
