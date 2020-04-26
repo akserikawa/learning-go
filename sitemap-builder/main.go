282b6bb5be5250ff9dcd36f9ab86d95d8d54d5f1
Akira Serikawa <akserikawa@gmail.com>
2020-04-26 20:15:53 +0200

    Filtering Links

--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -14,7 +15,14 @@ func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to be a sitemap for")
 	flag.Parse()
 
-	resp, err := http.Get(*urlFlag)
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
@@ -26,18 +34,35 @@ func main() {
 		Host:   reqUrl.Host,
 	}
 	base := baseUrl.String()
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
 
-	links, _ := link.Parse(resp.Body)
-	var hrefs []string
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
+			ret = append(ret, l.Href)
 		}
 	}
-	for _, href := range hrefs {
-		fmt.Println(href)
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
 	}
 }
